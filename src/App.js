483823b63c4a3a6ac38877ab4b8e4be70e3f1042
Petr <petr@Petrs-iMac.local>
2020-10-24 16:15:22 +0200

    changed text to 123 test

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          I was changed
+          123 test
         </p>
         <a
           className="App-link"
