de3fe6bb291b78dd1a8ddc4fb4daf98c521af09f
Petr <petr@Petrs-iMac.local>
2020-10-25 11:52:18 +0100

    branch test

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          123 test
+          branches
         </p>
         <a
           className="App-link"
